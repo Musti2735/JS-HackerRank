713363b66ad299a6f4926294811c1eb3c00fd983
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-05-31 17:12:07 +0300

    commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//Day 0: Hello, World
+
+let inputString = prompt("Plese enter a string: ")
+
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    console.log(inputString)
+}
+processData(inputString)
